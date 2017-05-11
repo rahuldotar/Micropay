5cd59bddfe06a90d9513bdae30558470a75cbdb9
Thuhin Sukesh <thuhin@accubits.com>
2017-05-11 10:33:23 +0530

    changes updated

--- server/services/coinbase.js
+++ server/services/coinbase.js
@@ -1,13 +1,18 @@
+/* Importing & initializing Coinbase Client[Start] */
 var CoinBaseClient = require('coinbase').Client;
 var cbc = new CoinBaseClient({'apiKey': 'API KEY', 
                          'apiSecret': 'API SECRET'});
+/* Impoting & initializing Coinbase Client[End] */                         
 
+/* get Exchange rates from coinbase client[Start] */
 var exchangeRates ={};
 cbc.getExchangeRates({'currency': 'BTC'}, function(err, rates) {
   exchangeRates = rates;    
   console.log(rates);
 });
+/* get Exchange rates from coinbase client[End] */
 
+/* Exporting the exchange rates[Start] */
 module.exports={
   getCurrencyExchangeRates: function(req,res){
      res.status(200).json({
@@ -15,3 +20,4 @@ module.exports={
     });
   }
 }
+/* Exporting the exchange rates[End] */
